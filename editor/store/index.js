c06aa7f7cac45e6d1c1c596924667c727f08bf24
Jorge <jorge.costa@developer.pt>
2018-01-16 20:58:46 +0000

    Implemented mechanism to allow reducers to customise what information gets serialised to localStorage; Changed mechanism to load defaults in loadAndPersist.

--- editor/store/index.js
+++ editor/store/index.js
@@ -6,7 +6,6 @@ import { registerReducer } from '@wordpress/data';
 /**
  * Internal dependencies
  */
-import { PREFERENCES_DEFAULTS } from './defaults';
 import reducer from './reducer';
 import { withRehydratation, loadAndPersist } from './persist';
 import enhanceWithBrowserSize from './mobile';
@@ -21,7 +20,7 @@ const STORAGE_KEY = `GUTENBERG_PREFERENCES_${ window.userSettings.uid }`;
 const store = applyMiddlewares(
 	registerReducer( 'core/editor', withRehydratation( reducer, 'preferences' ) )
 );
-loadAndPersist( store, 'preferences', STORAGE_KEY, PREFERENCES_DEFAULTS );
+loadAndPersist( store, reducer, 'preferences', STORAGE_KEY );
 enhanceWithBrowserSize( store, BREAK_MEDIUM );
 
 export default store;
